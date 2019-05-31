41a855af19d86efadce70aeafe4308a2ace81786
Radim Vansa <rvansa@redhat.com>
2019-05-31 14:44:59 +0200

    Minor fix

--- api/src/main/java/io/hyperfoil/api/session/ResourceUtilizer.java
+++ api/src/main/java/io/hyperfoil/api/session/ResourceUtilizer.java
@@ -4,6 +4,9 @@ public interface ResourceUtilizer {
    void reserve(Session session);
 
    static void reserve(Session session, Object... objects) {
+      if (objects == null) {
+         return;
+      }
       for (Object o : objects) {
          if (o instanceof ResourceUtilizer) {
             ((ResourceUtilizer) o).reserve(session);
