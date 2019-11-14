2e8fff0042c8a9a6995bf99546ae67a34d455d8f
Radim Vansa <rvansa@redhat.com>
2019-11-14 15:40:40 +0100

    Find best match for agent clustering IP

--- clustering/src/main/java/io/hyperfoil/Hyperfoil.java
+++ clustering/src/main/java/io/hyperfoil/Hyperfoil.java
@@ -3,10 +3,16 @@ package io.hyperfoil;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.Arrays;
+import java.util.BitSet;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.infinispan.commons.util.FileLookupFactory;
 import org.infinispan.configuration.parsing.ConfigurationBuilderHolder;
@@ -39,18 +45,23 @@ class Hyperfoil {
       options.getEventBusOptions().setClustered(true);
       try {
          String clusterIp = System.getProperty(AgentProperties.CONTROLLER_CLUSTER_IP);
-         String hostAddress;
+         InetAddress address;
          if (isController && clusterIp != null) {
-            hostAddress = InetAddress.getByName(clusterIp).getHostAddress();
+            address = InetAddress.getByName(clusterIp);
          } else {
-            hostAddress = InetAddress.getLocalHost().getHostAddress();
-            if (!isController && LOCALHOST_IPS.contains(hostAddress)) {
-               // bind agent to all interfaces
-               hostAddress = "0.0.0.0";
+            address = InetAddress.getLocalHost();
+            if (!isController && LOCALHOST_IPS.contains(address.getHostAddress())) {
+               InetAddress bestMatch = getAddressWithBestMatch(InetAddress.getByName(clusterIp));
+               if (bestMatch != null) {
+                  address = bestMatch;
+               } else {
+                  log.warn("No match found between controller IP and local addresses.");
+               }
             }
          }
-
-         log.info("Using host address {}", hostAddress);
+         String hostName = address.getHostName();
+         String hostAddress = address.getHostAddress();
+         log.info("Using host name {}/{}", hostName, hostAddress);
          if (LOCALHOST_IPS.contains(hostAddress) && clusterIp == null) {
             log.error("This machine is configured to resolve its hostname to 127.0.0.1; this is " +
                   "an invalid configuration for clustering. Make sure `hostname -i` does not return 127.0.0.1 or ::1 " +
@@ -85,6 +96,43 @@ class Hyperfoil {
          Codecs.register(vertx);
          startedHandler.handle(vertx);
       });
+   }
+
+   private static InetAddress getAddressWithBestMatch(InetAddress controllerAddress) {
+      InetAddress address = null;
+      try {
+         List<InetAddress> allAddresses = Collections.list(NetworkInterface.getNetworkInterfaces()).stream().filter(nic -> {
+            try {
+               return !nic.isLoopback() && nic.isUp();
+            } catch (SocketException e) {
+               log.warn("Error enumerating NIC {}", e, nic);
+               return false;
+            }
+         }).flatMap(nic -> Collections.list(nic.getInetAddresses()).stream()).collect(Collectors.toList());
+         log.info("Agent must choose NIC with best subnet match to controller ({}/{}), available IPs: {}",
+               controllerAddress.getHostName(), controllerAddress.getHostAddress(), allAddresses);
+         int longestMatch = -1;
+         BitSet controllerBits = BitSet.valueOf(controllerAddress.getAddress());
+         for (InetAddress a : allAddresses) {
+            if (a.getAddress().length != controllerAddress.getAddress().length) {
+               log.debug("Ignoring {} as this has different address length", a);
+               continue;
+            }
+            BitSet aBits = BitSet.valueOf(a.getAddress());
+            int i = 0;
+            while (i < aBits.length() && aBits.get(i) == controllerBits.get(i)) {
+               ++i;
+            }
+            log.debug("{} and {} have common prefix {} bits", controllerAddress, a, i);
+            if (i > longestMatch) {
+               longestMatch = i;
+               address = a;
+            }
+         }
+      } catch (SocketException e) {
+         log.warn("Error enumerating NICs", e);
+      }
+      return address;
    }
 
    private static DefaultCacheManager createCacheManager() {
