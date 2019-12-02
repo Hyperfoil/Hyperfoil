6f1c94957354c984c5198a9f7080b76955528503
Moss Uchida <super@192.168.0.13>
2019-12-02 18:50:11 -0500

    update

---
+++ distribution/src/main/java/io/hyperfoil/acmeair/LogHeaderHandler.java
@@ -0,0 +1,71 @@
+package io.hyperfoil.acmeair;
+
+
+
+
+import org.kohsuke.MetaInfServices;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.hyperfoil.api.config.Name;
+import io.hyperfoil.api.config.Step;
+import io.hyperfoil.api.connection.HttpRequest;
+import io.hyperfoil.api.http.HeaderHandler;
+import io.hyperfoil.api.session.Access;
+import io.hyperfoil.core.session.SessionFactory;
+import io.hyperfoil.function.SerializableSupplier;
+
+
+/**
+ * This is a debugging step.
+ */
+public class LogHeaderHandler implements HeaderHandler {
+   private static final Logger log = LoggerFactory.getLogger(LogHeaderHandler.class);
+
+   private final String message;
+   private Access var;
+
+   public LogHeaderHandler(String message, String var) {
+      this.message = message;
+      this.var = SessionFactory.access(var);
+   }
+
+   @Override
+   public void handleHeader(HttpRequest request, CharSequence header, CharSequence value) {
+       log.info("Message : " + message);
+       log.info("Var : " + var.getVar(request.session).objectValue());
+       //log.info("Var Object : " + var.getObject(request.session));
+   }
+
+   // Make this builder loadable as service
+   @MetaInfServices(HeaderHandler.Builder.class)
+   // This is the step name that will be used in the YAML
+   @Name("log")
+   public static class Builder implements HeaderHandler.Builder {
+      String message;
+      String var;
+
+      /**
+       * Message format pattern. Use <code>{}</code> to mark the positions for variables in the logged message.
+       *
+       * @param message Message format pattern.
+       * @return Self.
+       */
+      public Builder message(String message) {
+          this.message = message;
+          return this;
+       }
+
+      public Builder var(String var) {
+          this.var = var;
+          return this;
+       }
+
+      @Override
+    public HeaderHandler build(SerializableSupplier<? extends Step> step) {
+        return new LogHeaderHandler(message, var);
+    }
+   }
+
+
+}
---
+++ distribution/src/main/java/io/hyperfoil/acmeair/StoreHeaderHandler.java
@@ -0,0 +1,92 @@
+package io.hyperfoil.acmeair;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.kohsuke.MetaInfServices;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.hyperfoil.api.config.Name;
+import io.hyperfoil.api.config.Step;
+import io.hyperfoil.api.connection.HttpRequest;
+import io.hyperfoil.api.http.HeaderHandler;
+import io.hyperfoil.api.session.Access;
+import io.hyperfoil.api.session.ResourceUtilizer;
+import io.hyperfoil.api.session.Session;
+import io.hyperfoil.core.session.SessionFactory;
+import io.hyperfoil.function.SerializableSupplier;
+import io.netty.handler.codec.http.HttpHeaderNames;
+
+/**
+ * Extract cookies to variable
+ */
+public class StoreHeaderHandler implements HeaderHandler, ResourceUtilizer {
+    private static final Logger log = LoggerFactory.getLogger(StoreHeaderHandler.class);
+    private String name;
+    private Access toVar;
+
+    public StoreHeaderHandler(String name, String toVar) {
+        log.debug("StoreHeaderHandler constructor name param ###" +  name + "###");
+        this.name = name;
+        this.toVar = SessionFactory.access(toVar);
+    }
+
+    // Make this builder loadable as service
+    @MetaInfServices(HeaderHandler.Builder.class)
+    // This is the step name that will be used in the YAML
+    @Name("cookie")
+    public static class Builder implements HeaderHandler.Builder {
+        private String name;
+        private String toVar;
+
+        @Override
+        public HeaderHandler build(SerializableSupplier<? extends Step> param) {
+            // TODO Auto-generated method stub
+            return new StoreHeaderHandler(name, toVar);
+        }
+
+        public Builder name(String param) {
+            log.debug("StoreHeaderHandler.Builder.name param ###" +  param + "###");
+            this.name = param;
+            return this;
+         }
+
+        public Builder toVar(String param) {
+            log.debug("StoreHeaderHandler.Builder.toVar param ###" +  param + "###");
+            this.toVar = param;
+            return this;
+         }
+    }
+
+
+
+    @Override
+    public void handleHeader(HttpRequest request, CharSequence header, CharSequence value) {
+        // TODO Auto-generated method stub
+
+        if (HttpHeaderNames.SET_COOKIE.regionMatches(true, 0, header, 0, Math.min(header.length(), HttpHeaderNames.SET_COOKIE.length()))) {
+
+            log.debug("StoreHeaderHandler.Builder.handleHeader cookie key ###" +  name + "###");
+
+
+            if (value.toString().contains(name)) {
+
+                Pattern pattern = Pattern.compile("=(.*);");
+
+                Matcher matcher = pattern.matcher(value);
+                if (matcher.find()) {
+                    log.debug("StoreHeaderHandler.Builder.handleHeader " + name + " ###" +  matcher.group(1) + "###");
+                    toVar.setObject(request.session, matcher.group(1));
+                }
+            }
+         }
+    }
+
+
+    @Override
+    public void reserve(Session session) {
+        // TODO Auto-generated method stub
+        toVar.declareObject(session);
+    }
+}
