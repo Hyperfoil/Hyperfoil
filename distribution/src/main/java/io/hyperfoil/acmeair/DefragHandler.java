871fea38fecaf932867b839112a25b71f12e06fc
Moss Uchida <super@192.168.0.13>
2019-12-16 22:03:06 -0500

    Adding defrag service loadable

---
+++ distribution/src/main/java/io/hyperfoil/acmeair/DefragHandler.java
@@ -0,0 +1,71 @@
+package io.hyperfoil.acmeair;
+
+import org.kohsuke.MetaInfServices;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.hyperfoil.api.config.InitFromParam;
+import io.hyperfoil.api.config.Locator;
+import io.hyperfoil.api.config.Name;
+import io.hyperfoil.api.connection.Request;
+import io.hyperfoil.api.processor.Processor;
+import io.hyperfoil.api.processor.RequestProcessorBuilder;
+import io.hyperfoil.core.builders.ServiceLoadedBuilderProvider;
+import io.hyperfoil.core.handlers.DefragProcessor;
+
+public class DefragHandler extends DefragProcessor{
+
+
+    private static final Logger log = LoggerFactory.getLogger(DefragHandler.class);
+
+    public DefragHandler(Processor processor) {
+        super(processor);
+    }
+
+    // Make this builder loadable as service
+    @MetaInfServices(RequestProcessorBuilder.class)
+    // This is the step name that will be used in the YAML
+    @Name("defrag")
+    public static class Builder implements RequestProcessorBuilder, InitFromParam<Builder> {
+        private Locator locator;
+        private RequestProcessorBuilder processor;
+
+        @Override
+        public Builder setLocator(Locator locator) {
+           this.locator = locator;
+           return this;
+        }
+
+        @Override
+        public DefragHandler build() {
+            // TODO Auto-generated method stub
+            return new DefragHandler(processor.build());
+        }
+
+        public Builder processor(Processor<Request> processor) {
+            return processor(() -> processor);
+         }
+
+         public Builder processor(RequestProcessorBuilder processor) {
+            this.processor = processor;
+            return this;
+         }
+
+         /**
+          * Pass the selected parts to another processor.
+          *
+          * @return Builder.
+          */
+         public ServiceLoadedBuilderProvider<RequestProcessorBuilder> processor() {
+            return new ServiceLoadedBuilderProvider<>(RequestProcessorBuilder.class, locator, this::processor);
+         }
+
+        @Override
+        public Builder init(String param) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+    }
+
+}
