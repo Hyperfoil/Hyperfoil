eaadcf6b1b5d123d9ab6f3a2bb3d40b26a0ca4c5
Radim Vansa <rvansa@redhat.com>
2020-01-02 17:42:41 +0100

    Fix bug in LimitedPool

---
+++ api/src/test/java/io/hyperfoil/api/collection/LimitedPoolTest.java
@@ -0,0 +1,24 @@
+package io.hyperfoil.api.collection;
+
+import java.util.ArrayDeque;
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.junit.Test;
+
+public class LimitedPoolTest {
+   @Test
+   public void test() {
+      final int size = 16;
+      LimitedPool<Integer> pool = new LimitedPool<>(size, () -> 0);
+      ArrayDeque<Integer> queue = new ArrayDeque<>(size);
+      for (int i = 0; i < 10000; ++i) {
+         if (queue.isEmpty()) {
+            queue.push(pool.acquire());
+         } else if (queue.size() == size || ThreadLocalRandom.current().nextBoolean()) {
+            pool.release(queue.poll());
+         } else {
+            queue.push(pool.acquire());
+         }
+      }
+   }
+}
