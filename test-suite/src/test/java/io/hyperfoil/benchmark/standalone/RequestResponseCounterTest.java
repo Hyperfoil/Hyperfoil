a9f7196b877a82bc29a573c1849cf2f6e520508e
Radim Vansa <rvansa@redhat.com>
2020-04-20 10:23:30 +0200

    Fix compilation error

--- test-suite/src/test/java/io/hyperfoil/benchmark/standalone/RequestResponseCounterTest.java
+++ test-suite/src/test/java/io/hyperfoil/benchmark/standalone/RequestResponseCounterTest.java
@@ -100,7 +100,7 @@ public class RequestResponseCounterTest {
 
       AtomicLong actualNumberOfRequests = new AtomicLong(0);
       LocalSimulationRunner runner = new LocalSimulationRunner(benchmark,
-            (phase, isPhaseComplete, stepId, metric, snapshot, countDown) -> actualNumberOfRequests.addAndGet(snapshot.histogram.getTotalCount()), null);
+            (phase, stepId, metric, snapshot, countDown) -> actualNumberOfRequests.addAndGet(snapshot.histogram.getTotalCount()), null);
       runner.run();
 
       assertEquals(counter.get(), actualNumberOfRequests.get());
