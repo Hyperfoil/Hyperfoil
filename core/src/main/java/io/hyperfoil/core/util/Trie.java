1b1d83c502b072f9e4df8311f7017f801c676644
Radim Vansa <rvansa@redhat.com>
2021-01-27 14:12:45 +0100

    Improve trie printout in `inspect` command.

--- core/src/main/java/io/hyperfoil/core/util/Trie.java
+++ core/src/main/java/io/hyperfoil/core/util/Trie.java
@@ -3,22 +3,36 @@ package io.hyperfoil.core.util;
 import java.io.Serializable;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import io.hyperfoil.api.config.Visitor;
+
 public class Trie implements Serializable {
+   @Visitor.Invoke(method = "terms")
    private final Node[] firstNodes;
 
    public Trie(String... strings) {
       firstNodes = getNodes(Stream.of(strings).map(s -> s.getBytes(StandardCharsets.UTF_8)).collect(Collectors.toList()), 0);
    }
 
+   public Map<String, Integer> terms() {
+      TreeMap<String, Integer> map = new TreeMap<>();
+      for (Node n : firstNodes) {
+         n.writeTo(map, new byte[0]);
+      }
+      return map;
+   }
+
    private Node[] getNodes(List<byte[]> strings, int index) {
       // Quick and dirty impl...
-      Set<Byte> bytes = strings.stream().filter(Objects::nonNull).map(s -> s[index]).distinct().collect(Collectors.toSet());
+      Set<Byte> bytes = strings.stream().filter(Objects::nonNull).map(s -> s[index]).collect(Collectors.toSet());
       List<Node> nodes = new ArrayList<>();
       for (byte b : bytes) {
          List<byte[]> matching = new ArrayList<>();
@@ -79,6 +93,19 @@ public class Trie implements Serializable {
          this.b = b;
          this.terminal = terminal;
          this.nextNodes = nextNodes;
+      }
+
+      public void writeTo(Map<String, Integer> map, byte[] prefix) {
+         byte[] current = Arrays.copyOf(prefix, prefix.length + 1);
+         current[prefix.length] = b;
+         if (terminal >= 0) {
+            map.put(new String(current, StandardCharsets.UTF_8), terminal);
+         }
+         if (nextNodes != null) {
+            for (Node n : nextNodes) {
+               n.writeTo(map, current);
+            }
+         }
       }
    }
 }
