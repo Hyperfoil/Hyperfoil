4b14a8abc20de3a239f9b079b944b378d4aa4667
Radim Vansa <rvansa@redhat.com>
2021-11-11 09:47:57 +0100

    Expose `count` processor in YAML configuration

--- core/src/main/java/io/hyperfoil/core/handlers/CountRecorder.java
+++ core/src/main/java/io/hyperfoil/core/handlers/CountRecorder.java
@@ -1,7 +1,12 @@
 package io.hyperfoil.core.handlers;
 
+import org.kohsuke.MetaInfServices;
+
+import io.hyperfoil.api.config.InitFromParam;
+import io.hyperfoil.api.config.Name;
 import io.hyperfoil.api.processor.Processor;
 import io.hyperfoil.api.session.IntAccess;
+import io.hyperfoil.core.session.SessionFactory;
 import io.netty.buffer.ByteBuf;
 import io.hyperfoil.api.session.Session;
 
@@ -21,6 +26,40 @@ public class CountRecorder implements Processor {
    public void process(Session session, ByteBuf data, int offset, int length, boolean isLastPart) {
       if (isLastPart) {
          toVar.addToInt(session, 1);
+      }
+   }
+
+   /**
+    * Records number of parts this processor is invoked on.
+    */
+   @MetaInfServices(Processor.Builder.class)
+   @Name("count")
+   public static class Builder implements Processor.Builder, InitFromParam<Builder> {
+      private String toVar;
+
+      /**
+       * @param param Name of variable for the number of occurences.
+       * @return Self.
+       */
+      @Override
+      public Builder init(String param) {
+         return toVar(param);
+      }
+
+      /**
+       * Variable where to store number of occurrences.
+       *
+       * @param toVar Variable name.
+       * @return Self.
+       */
+      public Builder toVar(String toVar) {
+         this.toVar = toVar;
+         return this;
+      }
+
+      @Override
+      public Processor build(boolean fragmented) {
+         return new CountRecorder(SessionFactory.intAccess(toVar));
       }
    }
 }
