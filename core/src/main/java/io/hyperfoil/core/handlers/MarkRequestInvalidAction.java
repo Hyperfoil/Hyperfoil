d7d077b61d11aec56bc6cb15d90ca8b3d962a1e2
Radim Vansa <rvansa@redhat.com>
2021-11-11 09:48:26 +0100

    Action to mark current request as invalid

---
+++ core/src/main/java/io/hyperfoil/core/handlers/MarkRequestInvalidAction.java
@@ -0,0 +1,34 @@
+package io.hyperfoil.core.handlers;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.kohsuke.MetaInfServices;
+
+import io.hyperfoil.api.config.Name;
+import io.hyperfoil.api.connection.Request;
+import io.hyperfoil.api.session.Action;
+import io.hyperfoil.api.session.Session;
+
+public class MarkRequestInvalidAction implements Action {
+   private static final Logger log = LogManager.getLogger(MarkRequestInvalidAction.class);
+
+   @Override
+   public void run(Session session) {
+      Request request = session.currentRequest();
+      if (request == null) {
+         log.error("#{} No request in progress", session.uniqueId());
+      } else {
+         log.trace("#{} Marking request as invalid", session.uniqueId());
+         request.markInvalid();
+      }
+   }
+
+   @MetaInfServices(Action.Builder.class)
+   @Name("markRequestInvalid")
+   public static class Builder implements Action.Builder {
+      @Override
+      public Action build() {
+         return new MarkRequestInvalidAction();
+      }
+   }
+}
