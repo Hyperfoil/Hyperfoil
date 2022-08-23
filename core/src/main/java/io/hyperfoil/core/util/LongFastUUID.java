c471aa2c0a5dbf7daa6afb2981b249ceb51452f4
Radim Vansa <rvansa@redhat.com>
2022-08-23 10:08:09 +0200

    Do not use reflection to get fast UUIDs #237

--- core/src/main/java/io/hyperfoil/core/util/LongFastUUID.java
+++ core/src/main/java/io/hyperfoil/core/util/LongFastUUID.java
@@ -1,34 +1,39 @@
 package io.hyperfoil.core.util;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Generate UUID based on the following match: [0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}
  */
 public class LongFastUUID {
+   private static char[] DIGITS = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+
    private LongFastUUID() {
-
    }
 
-   private static Method fastUUID;
-   static {
-      try {
-         fastUUID = Long.class.getDeclaredMethod("fastUUID", long.class, long.class);
-      } catch (NoSuchMethodException e) {
-         throw new IllegalArgumentException(e);
-      }
-      fastUUID.setAccessible(true);
-   }
-
+   // Note: ideally we would use Long.fastUUID but that's not accessible in newer JDKs
    public static String randomUUID() {
-      try {
-         Random r = ThreadLocalRandom.current();
-         return (String) fastUUID.invoke(null, r.nextLong(), r.nextLong());
-      } catch (IllegalAccessException | InvocationTargetException e) {
-         throw new IllegalStateException("Cannot generate UUID", e);
+      ThreadLocalRandom random = ThreadLocalRandom.current();
+      long lsb = random.nextLong(), msb = random.nextLong();
+
+      char[] buf = new char[36];
+      set(msb >>> 32, buf, 0, 8);
+      buf[8] = '-';
+      set(msb >>> 16, buf, 9, 4);
+      buf[13] = '-';
+      set(msb, buf, 14, 4);
+      buf[18] = '-';
+      set(lsb >>> 48, buf, 19, 4);
+      buf[23] = '-';
+      set(lsb, buf, 24, 12);
+
+      return new String(buf);
+   }
+
+   private static void set(long value, char[] buf, int offset, int length) {
+      for (int index = offset + length - 1; index >= offset; --index) {
+         buf[index] = DIGITS[(int) (value & 0xF)];
+         value >>>= 4;
       }
    }
 }
