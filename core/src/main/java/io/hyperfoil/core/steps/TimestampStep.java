f953d35a4e8de2e89e2125753f585d50f2ed008c
Radim Vansa <rvansa@redhat.com>
2022-09-09 09:32:13 +0200

    Provide date format option for timestamp step #236

--- core/src/main/java/io/hyperfoil/core/steps/TimestampStep.java
+++ core/src/main/java/io/hyperfoil/core/steps/TimestampStep.java
@@ -6,29 +6,68 @@ import io.hyperfoil.api.config.Step;
 import io.hyperfoil.api.config.StepBuilder;
 import io.hyperfoil.api.config.BenchmarkDefinitionException;
 import io.hyperfoil.api.session.ObjectAccess;
+import io.hyperfoil.api.session.ResourceUtilizer;
 import io.hyperfoil.api.session.Session;
 import io.hyperfoil.core.builders.BaseStepBuilder;
 import io.hyperfoil.core.session.SessionFactory;
+import io.hyperfoil.function.SerializableSupplier;
 
 import org.kohsuke.MetaInfServices;
 
+import java.text.SimpleDateFormat;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+import java.util.stream.Stream;
 
 /**
  * This class implements a {@link Step} that bumps the current time in milliseconds as {@link String} to a variable.
  */
-public class TimestampStep implements Step {
+public class TimestampStep implements Step, ResourceUtilizer {
    private final ObjectAccess toVar;
+   private final Session.ResourceKey<FormatterResource> formatterKey;
+   private final SerializableSupplier<FormatterResource> formatterSupplier;
 
-   public TimestampStep(ObjectAccess toVar) {
+   public TimestampStep(ObjectAccess toVar, Session.ResourceKey<FormatterResource> formatterKey, SerializableSupplier<FormatterResource> formatterSupplier) {
       this.toVar = toVar;
+      this.formatterKey = formatterKey;
+      this.formatterSupplier = formatterSupplier;
    }
 
    @Override
    public boolean invoke(Session session) {
-      toVar.setObject(session, String.valueOf(System.currentTimeMillis()));
+      String timestamp;
+      if (formatterKey != null) {
+         timestamp = session.getResource(formatterKey).format.format(new Date());
+      } else {
+         timestamp = String.valueOf(System.currentTimeMillis());
+      }
+      toVar.setObject(session, timestamp);
       return true;
+   }
+
+   @Override
+   public void reserve(Session session) {
+      if (formatterKey != null) {
+         session.declareResource(formatterKey, formatterSupplier);
+      }
+   }
+
+   private static class FormatterResource implements Session.Resource {
+      private final SimpleDateFormat format;
+
+      public FormatterResource(String pattern, String localeCountry) {
+         Locale locale;
+         if (localeCountry == null || localeCountry.isBlank()) {
+            locale = Locale.US; // we need a deterministic choice
+         } else {
+            locale = Stream.of(Locale.getAvailableLocales())
+                  .filter(l -> localeCountry.equals(l.getCountry())).findFirst()
+                  .orElseThrow(() -> new IllegalArgumentException("No locale with 2-letter country code '" + localeCountry + "' available."));
+         }
+         this.format = new SimpleDateFormat(pattern, locale);
+      }
    }
 
    /**
@@ -38,6 +77,8 @@ public class TimestampStep implements Step {
    @Name("timestamp")
    public static class Builder extends BaseStepBuilder<Builder> implements InitFromParam<Builder> {
       private String toVar;
+      private String pattern;
+      private String localeCountry;
 
       /**
        * @param param Variable name.
@@ -59,12 +100,39 @@ public class TimestampStep implements Step {
          return this;
       }
 
+      /**
+       * Format the timestamp using <code>SimpleDateFormat</code> pattern.
+       *
+       * @param pattern Pattern for formatting.
+       * @return Self.
+       */
+      public Builder pattern(String pattern) {
+         this.pattern = pattern;
+         return this;
+      }
+
+      /**
+       * 2-letter ISO country code used in the formatter locale. Defaults to 'US'.
+       *
+       * @param localeCountry Country ISO code.
+       * @return Self.
+       */
+      public Builder localeCountry(String localeCountry) {
+         this.localeCountry = localeCountry;
+         return this;
+      }
+
       @Override
       public List<Step> build() {
          if (toVar == null) {
             throw new BenchmarkDefinitionException("Missing toVar attribute");
          }
-         return Collections.singletonList(new TimestampStep(SessionFactory.objectAccess(toVar)));
+         if (localeCountry != null && pattern == null) {
+            throw new BenchmarkDefinitionException("Country code is used only when the formatter pattern is set.");
+         }
+         return Collections.singletonList(new TimestampStep(SessionFactory.objectAccess(toVar),
+               pattern != null ? new Session.ResourceKey<>() {} : null,
+               pattern != null ? () -> new FormatterResource(pattern, localeCountry) : null));
       }
    }
 }
---
+++ core/src/test/java/io/hyperfoil/core/generators/TimestampStepTest.java
@@ -0,0 +1,34 @@
+package io.hyperfoil.core.generators;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+import io.hyperfoil.api.config.Locator;
+import io.hyperfoil.api.session.ObjectAccess;
+import io.hyperfoil.api.session.Session;
+import io.hyperfoil.core.session.SessionFactory;
+import io.hyperfoil.core.steps.TimestampStep;
+import io.hyperfoil.core.test.TestUtil;
+
+public class TimestampStepTest {
+   @Test
+   public void testTimestamp() {
+      Locator.push(TestUtil.locator());
+      try {
+         TimestampStep step = (TimestampStep) new TimestampStep.Builder().toVar("foo").pattern("yyyyy.MMMMM.dd GGG hh:mm aaa").build().get(0);
+         ObjectAccess foo = SessionFactory.objectAccess("foo");
+         Session session = SessionFactory.forTesting(foo);
+         step.reserve(session);
+         TestUtil.resolveAccess(session, step);
+
+         assertThat(step.invoke(session)).isTrue();
+         assertThat(foo.getObject(session)).matches(ts -> {
+            String timestamp = (String) ts;
+            return timestamp.matches("020[0-9][0-9].[a-zA-Z]+.[0-9][0-9] AD [0-9][0-9]:[0-9][0-9] [AP]M");
+         });
+      } finally {
+         Locator.pop();
+      }
+   }
+}
