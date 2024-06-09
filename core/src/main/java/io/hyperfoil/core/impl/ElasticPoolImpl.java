d325adf5129978a341320d656e8e2f161de8a479
Francesco Nigro <nigro.fra@gmail.com>
2024-06-09 13:52:16 +0200

    Uses JCTools primary lock-free queue on Session pooling

--- core/src/main/java/io/hyperfoil/core/impl/ElasticPoolImpl.java
+++ core/src/main/java/io/hyperfoil/core/impl/ElasticPoolImpl.java
@@ -1,12 +1,15 @@
 package io.hyperfoil.core.impl;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.atomic.LongAdder;
 import java.util.function.Supplier;
 
 import io.hyperfoil.api.collection.ElasticPool;
+import io.netty.util.internal.PlatformDependent;
+import org.jctools.queues.MpmcArrayQueue;
+import org.jctools.queues.MpmcUnboundedXaddArrayQueue;
+import org.jctools.queues.atomic.MpmcAtomicArrayQueue;
 
 public class ElasticPoolImpl<T> implements ElasticPool<T> {
    protected final LongAdder used = new LongAdder();
@@ -14,8 +17,10 @@ public class ElasticPoolImpl<T> implements ElasticPool<T> {
    private final Supplier<T> depletionSupplier;
    protected volatile int minUsed;
    protected volatile int maxUsed;
-   private ArrayBlockingQueue<T> primaryQueue;
-   private final BlockingQueue<T> secondaryQueue = new LinkedBlockingQueue<>();
+   private Queue<T> primaryQueue;
+   private final Queue<T> secondaryQueue = PlatformDependent.hasUnsafe() ?
+           new MpmcUnboundedXaddArrayQueue<>(128) :
+           new ConcurrentLinkedQueue<>();
 
    public ElasticPoolImpl(Supplier<T> initSupplier, Supplier<T> depletionSupplier) {
       this.initSupplier = initSupplier;
@@ -52,17 +57,39 @@ public class ElasticPoolImpl<T> implements ElasticPool<T> {
       secondaryQueue.add(object);
    }
 
-   @Override
-   public void reserve(int capacity) {
-      if (primaryQueue == null || primaryQueue.size() < capacity) {
-         primaryQueue = new ArrayBlockingQueue<>(capacity);
-      }
-      while (primaryQueue.size() < capacity) {
-         primaryQueue.add(initSupplier.get());
-      }
-   }
+    @Override
+    public void reserve(int capacity) {
+        if (primaryQueue == null) {
+            final int qCapacity = Math.max(2, capacity);
+            primaryQueue = PlatformDependent.hasUnsafe() ?
+                    new MpmcArrayQueue<>(qCapacity) :
+                    new MpmcAtomicArrayQueue<>(qCapacity);
+            for (int i = 0; i < capacity; i++) {
+                primaryQueue.offer(initSupplier.get());
+            }
+            assert primaryQueue.size() == capacity;
+            return;
+        }
+        final int currentCapacity = primaryQueue.size();
+        if (currentCapacity >= capacity) {
+            return;
+        }
+        var oldStorage = primaryQueue;
+        primaryQueue = null;
+        final int qCapacity = Math.max(2, capacity);
+        primaryQueue = PlatformDependent.hasUnsafe() ?
+                new MpmcArrayQueue<>(qCapacity) :
+                new MpmcAtomicArrayQueue<>(qCapacity);
+        for (int i = 0; i < currentCapacity; i++) {
+            primaryQueue.offer(oldStorage.poll());
+        }
+        for (int i = currentCapacity; i < capacity; i++) {
+            primaryQueue.offer(initSupplier.get());
+        }
+        assert primaryQueue.size() == capacity;
+    }
 
-   public void incrementUsed() {
+    public void incrementUsed() {
       used.increment();
       long currentlyUsed = used.longValue();
       if (currentlyUsed > maxUsed) {
@@ -83,14 +110,17 @@ public class ElasticPoolImpl<T> implements ElasticPool<T> {
       assert currentlyUsed >= 0;
    }
 
+   @Override
    public int minUsed() {
       return minUsed;
    }
 
+   @Override
    public int maxUsed() {
       return maxUsed;
    }
 
+   @Override
    public void resetStats() {
       int current = used.intValue();
       minUsed = current;
