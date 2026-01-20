3d430c01cfded6cabcbcf4d9d4b4864cc95cbbd2
Diego Lovison <diegolovison@gmail.com>
2026-01-20 09:24:22 -0300

    Refactor: extract scenario creation from command logic

--- cli/src/main/java/io/hyperfoil/cli/commands/Wrk2.java
+++ cli/src/main/java/io/hyperfoil/cli/commands/Wrk2.java
@@ -53,19 +53,8 @@ public class Wrk2 extends WrkAbstract {
       int rate;
 
       @Override
-      protected PhaseBuilder<?> phaseConfig(PhaseBuilder.Catalog catalog, PhaseType phaseType, long durationMs) {
-         int durationSeconds = (int) Math.ceil(durationMs / 1000);
-         int maxSessions = switch (phaseType) {
-            // given that the duration of this phase is 6s seconds
-            // there's no point to have more than 6 * rate sessions
-            case calibration -> rate * durationSeconds;
-            case test -> rate * 15;
-         };
-         return catalog.constantRate(rate)
-               .variance(false)
-               .maxSessions(maxSessions);
+      protected PhaseBuilder<?> phaseConfig(PhaseBuilder.Catalog catalog, WrkScenario.PhaseType phaseType, long durationMs) {
+         return WrkScenarioPhaseConfig.wrk2PhaseConfig(catalog, phaseType, durationMs, rate);
       }
-
    }
-
 }
