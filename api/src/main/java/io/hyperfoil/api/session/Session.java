5fdfe3b22821aeeb07a02612658f7c12267b0c7d
Diego Lovison <diegolovison@gmail.com>
2026-06-26 13:19:08 -0300

    return Future on proceed() and defer tryTerminate

--- api/src/main/java/io/hyperfoil/api/session/Session.java
+++ api/src/main/java/io/hyperfoil/api/session/Session.java
@@ -9,6 +9,7 @@ import io.hyperfoil.api.connection.Request;
 import io.hyperfoil.api.statistics.SessionStatistics;
 import io.hyperfoil.api.statistics.Statistics;
 import io.netty.util.concurrent.EventExecutor;
+import io.vertx.core.Future;
 
 public interface Session {
 
@@ -105,7 +106,7 @@ public interface Session {
    /**
     * Run anything that can be executed.
     */
-   void proceed();
+   Future<Void> proceed();
 
    void reset();
 
