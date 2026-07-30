8c3d8199d70dec53f5a2244f7e930308c013d77b
Diego Lovison <diegolovison@gmail.com>
2026-07-30 15:25:16 -0300

    HotRodErgonomics extends Ergonomics

--- hotrod/src/main/java/io/hyperfoil/hotrod/config/HotRodErgonomics.java
+++ hotrod/src/main/java/io/hyperfoil/hotrod/config/HotRodErgonomics.java
@@ -1,4 +1,16 @@
 package io.hyperfoil.hotrod.config;
 
-public class HotRodErgonomics {
+import io.hyperfoil.api.config.Ergonomics;
+
+public class HotRodErgonomics extends Ergonomics {
+
+   private final HotRodPluginBuilder parent;
+
+   public HotRodErgonomics(HotRodPluginBuilder parent) {
+      this.parent = parent;
+   }
+
+   public HotRodPluginBuilder endErgonomics() {
+      return parent;
+   }
 }
